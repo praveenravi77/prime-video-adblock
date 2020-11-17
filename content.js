7ca14359708764523fb8ddba1e78fcd9dd664dff
praveenravi77 <praveenravi77@gmail.com>
2020-11-17 09:42:40 -0500

    spaghetti code to try and avoid losing content

--- content.js
+++ content.js
@@ -9,21 +9,50 @@
     attributeOldValue: false
   }
   
+  var lastAdTime = -1;
+
   function skipAd(mutations) {
     let videoNode = document.querySelector("#dv-web-player > div > div:nth-child(1) > div > div > div.scalingVideoContainer > div.scalingVideoContainerBottom > div > video");
     let adText = document.querySelector(".atvwebplayersdk-adtimeindicator-text");
   
     if (videoNode != null && adText != null) {
       let adDuration = parseInt(adText.textContent.match(/^\d+|\d+\b|\d+(?=\w)/g)[0]);
-      videoNode.currentTime = videoNode.currentTime + adDuration;
-  
-      console.log(`${adDuration} Second ad detected and skiped`);
+
+      console.log(`${adDuration} Second ad detected`);
+
+      let curr = videoNode.currentTime;
+      let afterAd = curr + adDuration;
+
+      // handle back to back ad wonkyness
+      if (curr - lastAdTime > 1) {
+        console.log("First ad");
+        videoNode.currentTime = afterAd;
+
+        console.log(`Time before ad: ${curr}`);
+        console.log(`Time after ad: ${videoNode.currentTime}`);
+
+        lastAdTime = afterAd;
+        console.log(`lastAdTime: ${lastAdTime}`)
+      } else {
+        console.log("Second back to back ad");
+        console.log(`Time before ad: ${curr}`);
+        videoNode.currentTime = afterAd;
+        setTimeout(() => { 
+          videoNode.currentTime = afterAd - adDuration;
+          lastAdTime = -1;
+          console.log(`Time after ad: ${videoNode.currentTime}`);
+          console.log(`lastAdTime: ${lastAdTime}`)
+        }, 100);
+      }
+
+      console.log(`${adDuration} Second ad skipped`);
     }
   }
   
   globalThis.AmazonAdObserver = new MutationObserver(skipAd);
 
   // let nodeToWatch = document.querySelector(".atvwebplayersdk-infobar-container");
+  // let nodeToWatch = document.querySelector(("#dv-web-player");
   let nodeToWatch = document.body;
   if (nodeToWatch != null) {
     console.log("successfully attached observer")
