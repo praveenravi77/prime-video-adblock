09a5172ade623fe899cdb71e50bed13014c6ae76
praveen <praveenravi77@gmail.com>
2020-11-20 23:39:19 -0500

    remove debug console logs, seems good enough to release

--- content.js
+++ content.js
@@ -18,34 +18,21 @@
     if (videoNode != null && adText != null) {
       let adDuration = parseInt(adText.textContent.match(/^\d+|\d+\b|\d+(?=\w)/g)[0]);
 
-      console.log(`${adDuration} Second ad detected`);
-
       let curr = videoNode.currentTime;
       let afterAd = curr + adDuration;
 
       // handle back to back ad wonkyness
       if (Math.abs(curr - lastAdTime) > 1) {
-        console.log("First ad");
         videoNode.currentTime = afterAd;
 
-        console.log(`Time before ad: ${curr}`);
-        console.log(`Time after ad: ${videoNode.currentTime}`);
-
         lastAdTime = afterAd;
-        console.log(`lastAdTime: ${lastAdTime}`)
       } else {
-        console.log("Second back to back ad");
-        console.log(`Time before ad: ${curr}`);
         videoNode.currentTime = afterAd;
         setTimeout(() => { 
           videoNode.currentTime = afterAd - adDuration;
           lastAdTime = -1;
-          console.log(`Time after ad: ${videoNode.currentTime}`);
-          console.log(`lastAdTime: ${lastAdTime}`)
         }, 100);
       }
-
-      console.log(`${adDuration} Second ad skipped`);
     }
   }
   
@@ -55,11 +42,7 @@
   // let nodeToWatch = document.querySelector(("#dv-web-player");
   let nodeToWatch = document.body;
   if (nodeToWatch != null) {
-    console.log("successfully attached observer")
     globalThis.AmazonAdObserver.observe(nodeToWatch, options);
-  } else {
-    console.log("Failed to find node to watch :(")
   }
 
-  console.log("Prime video adblock init completed.");
 })();
